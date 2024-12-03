d3fe86bb84cea47caaae2c82fface84d1c45c66c
ngamisolatedirl <viplata12@gmail.com>
2024-12-03 08:36:41 +0700

    spa 3

--- src/main/java/com/fptaptech/s4/service/interfaces/IShuttleBookingService.java
+++ src/main/java/com/fptaptech/s4/service/interfaces/IShuttleBookingService.java
@@ -8,14 +8,10 @@ public interface IShuttleBookingService {
 
     Response findBookingByConfirmationCode(Long branchId, String confirmationCode);
 
+    Response getAllShuttles(Long branchId);
 
     Response cancelShuttleBooking(Long branchId, Long shuttleBookingId);
 
-    String getBookingEmail(Long branchId, Long shuttleBookingId);
-
-    Response getAllShuttleBookings(Long branchId);
-
     Response getAllShuttleBookingsByUser(Long userId);
-
-    Response getAllShuttles(Long branchId);
+    String getBookingEmail(Long branchId, Long shuttleBookingId);
 }
